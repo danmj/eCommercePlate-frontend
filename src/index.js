b69aa15105eeb48b280a1fdbd16b757af2b2fcc4
danmj <daniel.jung@stern.nyu.edu>
2019-09-15 16:05:51 -0400

    devtools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ import combineReducers from './reducers/index.js';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-const store = createStore(combineReducers, compose(applyMiddleware(thunk)))
+const store = createStore(
+  combineReducers,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 ReactDOM.render(
   <Provider store={store}>
